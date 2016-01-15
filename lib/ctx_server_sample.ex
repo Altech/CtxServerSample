0c0727bc63c5be7aa7bd3857b926428562978167
Altech <takeno.sh@gmail.com>
2016-01-15 17:28:04 +0900

    Import CtxServer module

--- lib/ctx_server_sample.ex
+++ lib/ctx_server_sample.ex
@@ -1,2 +1,57 @@
 defmodule CtxServerSample do
+  use CtxServer
+
+  defmacro debug_info(request) do
+    quote do
+      {name, arity} = __ENV__.function
+      IO.puts """
+      CALL: #{name}/#{arity}
+        request: #{unquote(request)},
+        contexts: #{inspect CtxServer.Contexts.current}
+      """
+    end
+  end
+
+  context login: true, payment: :normal do
+    def handle_cast(request, state) do
+      debug_info(request)
+      {:noreply, state}
+    end
+
+    def handle_call(request, _, state) do
+      debug_info(request)
+      {:reply, request, state}
+    end
+  end
+
+  context login: true, payment: :abnormal do
+    def handle_cast(request, state) do
+      debug_info(request)
+      {:noreply, state}
+    end
+
+    def handle_call(request, _, state) do
+      debug_info(request)
+      {:reply, request, state}
+    end
+  end
+
+  def handle_cast(request, state) do
+    debug_info(request)
+    {:noreply, state}
+  end
+
+  def handle_call(request, from, state) do
+    debug_info(request)
+    {:reply, request, state}
+  end
+end
+
+
+quote do
+  # CtxServer.switch_context :login, true
+  {:ok, pid} = CtxServer.start(CtxServerSample, [])
+  CtxServer.Contexts.update login: true, payment: :normal
+  CtxServer.cast(pid, :foo)
+  CtxServer.call(pid, :foo)
 end

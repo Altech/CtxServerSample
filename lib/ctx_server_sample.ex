ee31e19f5754959466897d90ee21e01d30d4bf34
Altech <takeno.sh@gmail.com>
2016-01-20 00:37:54 +0900

    Rename PlugStarter

--- lib/ctx_server_sample.ex
+++ lib/ctx_server_sample.ex
@@ -1,19 +1,15 @@
 defmodule CtxServerSample do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
       supervisor(CtxServerSample.Repo, []),
-      worker(CtxServerSample.Plugstarter, []),
+      worker(CtxServerSample.ProxyServer, []), 
       worker(CtxServerSample.HTTPServer, [HTTPServer]),
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: CtxServerSample.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- lib/ctx_server_sample/proxy_server.ex
+++ lib/ctx_server_sample/proxy_server.ex
@@ -1,7 +1,5 @@
-defmodule CtxServerSample.Plugstarter do
-
+defmodule CtxServerSample.ProxyServer do
   def start_link() do
     {:ok, _} = Plug.Adapters.Cowboy.http CtxServerSample.Router, []
   end
-
 end

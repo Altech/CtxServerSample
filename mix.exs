403c0d7345dd2d3a50756cbc6354958da87b9e6b
Altech <takeno.sh@gmail.com>
2016-01-19 15:40:30 +0900

    Describe about libraries

--- mix.exs
+++ mix.exs
@@ -24,10 +24,10 @@ defmodule CtxServerSample.Mixfile do
       "git@github.com:psg-titech/CtxServer.git"
     end
     [
-      {:ctx_server, git: ctx_server},
-      {:cowboy, "~> 1.0.0"},
-      {:plug, "~> 1.1.0"},
-      {:eml, git: "https://github.com/zambal/eml.git"},
+      {:ctx_server, git: ctx_server}, # Context-Aware GenServer
+      {:cowboy, "~> 1.0.0"}, # HTTP Server
+      {:plug, "~> 1.1.0"}, # Functional HTTP Processing
+      {:eml, git: "https://github.com/zambal/eml.git"}, # HTML Template Engine
     ]
   end
 end

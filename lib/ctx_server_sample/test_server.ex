dda636511c5f8ac17b23bbba528ad30959b80ba1
Altech <takeno.sh@gmail.com>
2016-01-19 21:22:27 +0900

    Remove useless code

--- lib/ctx_server_sample/test_server.ex
+++ lib/ctx_server_sample/test_server.ex
@@ -8,17 +8,6 @@ defmodule CtxServerSample.TestServer do
     CtxServer.start_link(__MODULE__, name, name: name)
   end
 
-  defmacro debug_info(request) do
-    quote do
-      {name, arity} = __ENV__.function
-      """
-      CALL: #{name}/#{arity}
-        request: #{inspect unquote(request)},
-        contexts: #{inspect CtxServer.Contexts.current}
-      """
-    end
-  end
-
   def handle_call({:get, :root, _, %{user_id: user_id}}, _, state) do
     use Eml.HTML
     links = ~w[login logout items]
@@ -99,46 +88,4 @@ defmodule CtxServerSample.TestServer do
     end
     {:reply, dom |> Eml.compile, state}
   end
-
-  context login: true, payment: :normal do
-    def handle_cast(request, state) do
-      IO.puts debug_info(request)
-      {:noreply, state}
-    end
-
-    def handle_call(request, _, state) do
-      {:reply, debug_info(request), state}
-    end
-  end
-
-  context login: true, payment: :abnormal do
-    def handle_cast(request, state) do
-      IO.puts debug_info(request)
-      {:noreply, state}
-    end
-
-    def handle_call(request, _, state) do
-      {:reply, debug_info(request), state}
-    end
-  end
-
-  context :any do
-    def handle_cast(request, state) do
-      IO.puts debug_info(request)
-      {:noreply, state}
-    end
-
-    def handle_call(request, from, state) do
-      {:reply, debug_info(request), state}
-    end
-  end
-end
-
-
-quote do
-  # CtxServer.switch_context :login, true
-  {:ok, pid} = CtxServer.start(CtxServerSample.TestServer, [])
-  CtxServer.Contexts.update login: true, payment: :normal
-  CtxServer.cast(pid, :foo)
-  CtxServer.call(pid, :foo)
 end
